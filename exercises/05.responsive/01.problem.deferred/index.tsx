3674af93223b5e4e8b52920f8ee2fbff8b5e8760
Wyatt Bai <wyatt.bai@caremappr.com.au>
2024-11-12 17:18:44 +1030

    feat: useDeferredValue

--- exercises/05.responsive/01.problem.deferred/index.tsx
+++ exercises/05.responsive/01.problem.deferred/index.tsx
@@ -1,4 +1,4 @@
-import { Suspense, use, useState, useTransition } from 'react'
+import { Suspense, use, useDeferredValue, useState, useTransition } from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { ErrorBoundary } from 'react-error-boundary'
 import { useSpinDelay } from 'spin-delay'
@@ -58,10 +58,10 @@ function ShipSearch({
 }) {
 	const [search, setSearch] = useState('')
 	// 🐨 remove the useTransition
-	const [isTransitionPending, startTransition] = useTransition()
+	const deferredSearch = useDeferredValue(search)
 	// 🐨 call useDeferredValue with the search
 	// 🐨 update the argument passed to useSpinDelay to be search !== deferredSearch
-	const isPending = useSpinDelay(isTransitionPending, {
+	const isPending = useSpinDelay(search !== deferredSearch, {
 		delay: 300,
 		minDuration: 350,
 	})
@@ -74,7 +74,7 @@ function ShipSearch({
 					value={search}
 					onChange={(event) => {
 						// 🐨 remove the startTransition wrapper here
-						startTransition(() => setSearch(event.currentTarget.value))
+						setSearch(event.currentTarget.value)
 					}}
 				/>
 			</div>
@@ -88,7 +88,7 @@ function ShipSearch({
 				<ul style={{ opacity: isPending ? 0.6 : 1 }}>
 					<Suspense fallback={<SearchResultsFallback />}>
 						{/* 🐨 pass the deferredSearch here */}
-						<SearchResults search={search} onSelection={onSelection} />
+						<SearchResults search={deferredSearch} onSelection={onSelection} />
 					</Suspense>
 				</ul>
 			</ErrorBoundary>
